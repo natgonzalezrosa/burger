a333c5a5f2f7fde407163f09f64b13e15c795fda
natgonzalezrosa <natgonzalezrosa@gmail.com>
2018-08-13 23:31:05 -0400

    added schema and seeds files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Big Mac", true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Krabby Patty", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bob's Burgers", true);
