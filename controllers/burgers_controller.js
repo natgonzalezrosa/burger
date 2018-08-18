725cc30d207462509319c8aaf26c490d81f5ba39
natgonzalezrosa <natgonzalezrosa@gmail.com>
2018-08-18 00:40:17 -0400

    updated index.handlebars

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,51 @@
+// Imports Express application object
+var express = require("express");
+
+// And uses it to get a Router object
+var router = express.Router();
+
+// Import the model (burger.js) to use its database functions
+var burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required
+
+// Home page route
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+// Route to insert a new burger
+router.post("/burgers/insertOne", function(req, res) {
+    burger.insertOne(["burger_name"], [req.body.burger_name], function(result) {
+        
+        // Redirect to the index page
+        res.redirect("/");
+    });
+});
+
+// Route to update burger once it's devoured
+router.put("/burgers/updateOne:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
+
+    burger.updateOne(
+        {
+            devoured: req.body.devoured
+        },
+        condition,
+        function(result) {
+            
+            // Redirect to the index page
+            res.redirect("/");
+        }
+    );
+});
+
+// Export routes for server.js to use
+module.exports = router;
