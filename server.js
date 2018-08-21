89da7c579e3ee95ab7f1bb5da56921b480a164d5
natgonzalezrosa <natgonzalezrosa@gmail.com>
2018-08-20 21:41:19 -0400

    fixed

--- server.js
+++ server.js
@@ -26,7 +26,8 @@ app.use("/", routes);
 var PORT = process.env.PORT || 3000;
 
 // Start our server so that it can begin listening to client requests
-app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on http://localhost:" + PORT);
-})
+// app.listen(PORT, function() {
+//     // Log (server-side) when our server has started
+//     console.log("Server listening on http://localhost:" + PORT);
+// })
+app.listen(PORT);
